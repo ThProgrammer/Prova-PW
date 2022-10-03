5de5f2db5d5592bbd826009eea52a5c3b12cee21
Thomas Ferreira <thomas.devfs@gmail.com>
2022-10-03 09:49:11 -0300

    Add project react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Imc from "./components/Imc"
+
+function App() {
+  return (
+    <div>
+      <Imc/>
+    </div>
+  );
+}
+
+export default App;
