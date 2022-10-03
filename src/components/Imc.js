56b4519baab9ff2af0862f1a9a759a3ee528679f
Thomas Ferreira <thomas.devfs@gmail.com>
2022-10-03 11:18:29 -0300

    Exercício 1 pronto
    Aprendizado: useState funciona em condição (mas só as vezes)

--- src/components/Imc.js
+++ src/components/Imc.js
@@ -2,50 +2,39 @@ import {useState} from "react";
 
 function Imc() {
 
-    const [peso, setPeso] = useState();
-    const [altura, setAltura] = useState();
     const [resultado, setResultado] = useState();
-    const [print, setPrint] = useState("______");
-
-    function handleRegister(e) {
-        var calcprint;
-        e.preventDefault();
-        
-        setResultado(peso/(altura*altura));
-
-        if (resultado < 18.5) {
-            calcprint = "Abaixo do peso";
-        }
-        if (resultado > 18.5) {
-            calcprint = "Normal";
-        }
-
-    } 
-
-
+    const [imc, setImc] = useState();
     
+
+    function calcimc (e) {
+        e.preventDefault();
+
+        if (imc < 18.5) {
+            setResultado("Abaixo do peso");
+        } else if (imc > 18.5 && imc < 24.9) {
+            setResultado("Normal");
+        } else if (imc > 24.9 && imc < 29.9) {
+            setResultado("Sobrepeso");
+        } else if (imc > 29.9 && imc < 34.9) {
+            setResultado("Obesidade Grau I");
+        } else  if (imc > 34.9 && imc < 39.9) {
+            setResultado("Obesidade Grau II")
+        } else {
+            setResultado("Obesidade Mórbida");
+        }
+    } 
 
     return (
         <>
-        <form onSubmit={handleRegister}>
-            <label>Peso</label>
-            <input onChange={(e) => {
-                setPeso(e.target.value);
-            }}/>
-            <br/>
-            <label>Altura</label>
-            <input onChange={(e) => {
-                setAltura(e.target.value);
-            }}/>
-            <br/>
-            <input type='submit'/>
-        </form>
+            <form onSubmit={calcimc}>
+                <label>IMC: </label>
+                <input onChange={(e) => {setImc(e.target.value);}}/> <br/>
 
-        <p>Resultado: {resultado}</p>
-        <p>Você está {print}</p>
+                <input type='submit'/>
+            </form>
+
+            <p>Resultado: {resultado}</p>
         </>
-
-
     )
     
 }
